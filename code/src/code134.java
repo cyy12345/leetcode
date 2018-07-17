b56c6fb613e607f58b83c72dbbd703ef085733cf
cyy <892471766@qq.com>
2018-07-18 00:21:46 +0800

    43

---
+++ code/src/code134.java
@@ -0,0 +1,34 @@
+public class code134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gassum=0;
+        int costsum =0;
+        int sum =0;
+        for(int i=0;i<gas.length;i++){
+            gassum+=gas[i];
+            costsum+=cost[i];
+            sum+=i;
+        }
+        if (costsum>gassum){
+            return -1;
+        }
+
+        int startindex = 0;
+        for(int i=0;i<gas.length;i++){
+            int gasincar = 0;
+            for(int j=0;j<gas.length;j++){
+                int index = j+i;
+                if(index>gas.length-1){
+                    index = index - gas.length;
+                }
+                gasincar+=gas[index];
+                if(gasincar<cost[index]){
+                    sum-=i;
+                    break;
+                }
+                gasincar-=cost[index];
+
+            }
+        }
+        return sum;
+    }
+}
