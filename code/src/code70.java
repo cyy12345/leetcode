4683e04794cdfdfeb1d9ee858029ab9bf1a3cb98
cyy <892471766@qq.com>
2018-08-01 17:32:09 +0800

    70

---
+++ code/src/code70.java
@@ -0,0 +1,34 @@
+public class code70 {
+    public int climbStairs(int n) {
+        int[] mem = new int[n+1];
+
+        return recurse(0,n,mem);
+    }
+
+    public int recurse(int i,int n,int[] mem){
+        if(i>n){
+            return 0;
+        }
+        if(i==n){
+            return 1;
+        }
+        if(mem[i]>0){
+            return mem[i];
+        }
+        mem[i] = recurse(i+1,n,mem)+recurse(i+2,n,mem);
+        return mem[i];
+    }
+
+    public int climbStairs1(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
