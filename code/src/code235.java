76f3ccdd5f2c5875028baa43993c8191fe45b45a
cyy <892471766@qq.com>
2018-08-09 12:10:20 +0800

    235

---
+++ code/src/code235.java
@@ -0,0 +1,24 @@
+public class code235 {
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as
+the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ */
+     // Definition for a binary tree node.
+      public class TreeNode {
+         int val;
+          TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+      }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while ((root.val - p.val) * (root.val - q.val) > 0)
+            root = p.val < root.val ? root.left : root.right;
+        return root;
+
+    }
+}
