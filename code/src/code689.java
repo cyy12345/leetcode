365c79b80ba74458c0b5c55935308eb5cb608d27
cyy <892471766@qq.com>
2018-08-28 17:34:20 +0800

    689

---
+++ code/src/code689.java
@@ -0,0 +1,48 @@
+public class code689 {
+    public int[] maxSumOfThreeSubarrays(int[] nums, int K) {
+        /*
+        In a given array nums of positive integers, find three non-overlapping subarrays with maximum sum.
+
+Each subarray will be of size k, and we want to maximize the sum of all 3*k entries.
+
+Return the result as a list of indices representing the starting position of each interval (0-indexed). If there are
+ multiple answers, return the lexicographically smallest one.
+         */
+
+        //W is an array of sums of windows
+        int[] W = new int[nums.length - K + 1];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (i >= K) sum -= nums[i-K];
+            if (i >= K-1) W[i-K+1] = sum;
+        }
+
+        int[] left = new int[W.length];
+        int best = 0;
+        for (int i = 0; i < W.length; i++) {
+            if (W[i] > W[best]) best = i;
+            left[i] = best;
+        }
+
+        int[] right = new int[W.length];
+        best = W.length - 1;
+        for (int i = W.length - 1; i >= 0; i--) {
+            if (W[i] >= W[best]) best = i;
+            right[i] = best;
+        }
+
+        int[] ans = new int[]{-1, -1, -1};
+        for (int j = K; j < W.length - K; j++) {
+            int i = left[j - K], k = right[j + K];
+            if (ans[0] == -1 || W[i] + W[j] + W[k] >
+                    W[ans[0]] + W[ans[1]] + W[ans[2]]) {
+
+                ans[0] = i;
+                ans[1] = j;
+                ans[2] = k;
+            }
+        }
+        return ans;
+    }
+}
